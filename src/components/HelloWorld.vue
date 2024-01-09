8db450d4d700e6cd4bb03a4c1df3ffdc2552b2c7
unknown <santiagoryan788@gmail.com>
2024-01-09 10:49:27 +0800

    new update 1/9/24

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,160 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div id="app">
+
+    <h1>Data from SQLite</h1>
+
+    <div>
+      <input v-model="qrInput" placeholder="Enter QRCODE" />
+      <!-- <button @click="fetchEmployeeAttendance()">Display Full Name</button> -->
+      <!-- <p>Full Name: {{ fullName }}</p>
+      <p>Time: {{ inputTime }}</p> -->
+      <button @click="insertData()">Insert Data</button>
+    </div>
+
+    <!-- <h2>Employee Attendance</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>QRCODE</th>
+          <th>TIME</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in employee - attendance" :key="item.id">
+          <td>{{ item.firstname.item.lastname }}</td>
+          <td>{{ item.department_name }}</td>
+          <td>{{ item.position }}</td>
+          <td>{{ item.time_in }}</td>
+          <td>{{ item.time_out }}</td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h2>Student Attendance</h2>
+    <table>
+      <thead>
+        <tr>
+          <th>QRCODE</th>
+          <th>FULL NAME</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="item in student - attendace" :key="item.id">
+          <td>{{ item.firstname.item.lastname }}</td>
+          <td>{{ item.time_in }}</td>
+          <td>{{ item.time_out }}</td>
+        </tr>
+      </tbody>
+    </table> -->
+
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
-</script>
+  // data() {
+  //   return {
+  //     attendanceData: [],
+  //     fullName: '',
+  //     inputTime: '',
+  //   };
+  // },
+  mounted() {
+    // this.fetchEmployeeAttendance();
+    // this.fetchStudentAttendance();
+    this.insertData();
+  },
+  methods: {
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+    // fetchEmployeeAttendance() {
+    //   fetch('http://localhost:3000/api/employee-account')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       this.attendanceData = data;
+    //     })
+    //     .catch(error => { 
+    //       console.error(error);
+    //     });
+    // },
+    // fetchStudentAttendance() {
+    //   fetch('http://localhost:3000/api/student-account')
+    //     .then(response => response.json())
+    //     .then(data => {
+    //       this.attendanceData = data;
+    //     })
+    //     .catch(error => {
+    //       console.error(error);
+    //     });
+    // },
+
+    insertData() {
+      if (this.qrInput) {
+        const currentDate = new Date().toISOString().slice(0, 19).replace('T', ' ');
+        this.inputTime = currentDate;
+        const data = {
+          QRCODE: this.qrInput,
+          TIME: this.inputTime,
+        };
+
+        fetch('http://localhost:3000/api/insert-attendance', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(data),
+        })
+          .then(response => {
+            if (response.ok) {
+              alert('Data inserted successfully');
+            } else {
+              throw new Error('Failed to insert data');
+            }
+          })
+      } else {
+        alert('Please provide QR Code and Time');
+      }
+    },
+
+    // displayFullName() {
+    //   const currentDate = new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+    //   this.inputTime = currentDate;
+
+    //   const foundItem = this.regularData.find(
+    //     item => item.QRCODE === this.qrInput
+    //   );
+    //   this.fullName = foundItem ? foundItem.FULLNAME : 'Not found';
+    // },
+    // insertData() {
+    //   if (this.qrInput && this.inputTime) {
+    //     const data = {
+    //       QRCODE: this.qrInput,
+    //       TIME: this.inputTime,
+    //     };
+
+    //     fetch('http://localhost:3000/api/insert-attendance', {
+    //       method: 'POST',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify(data),
+    //     })
+    //       .then(response => {
+    //         if (response.ok) {
+    //           alert('Data inserted successfully');
+    //           this.fetchAttendanceData();
+    //         } else {
+    //           throw new Error('Failed to insert data');
+    //         }
+    //       })
+    //       .catch(error => {
+    //         console.error('Error:', error);
+    //         alert('Failed to insert data');
+    //       });
+    //   } else {
+    //     alert('Please provide QR Code and Time');
+    //   }
+    // },
+  },
+};
+</script>
